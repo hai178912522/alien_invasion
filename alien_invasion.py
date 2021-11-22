08eb0c484e043d180c46d8ff79f218b712957782
张海浩 <zhanghaihao@singleronbio.com>
2021-11-22 17:41:50 +0800

    13.4.4 向下移动外星人群并改变移动方向

--- alien_invasion.py
+++ alien_invasion.py
@@ -125,8 +125,22 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """有外星人到达边缘时采取相应的措施"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """将整群外星人下移，并改变它们的方向。"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_aliens(self):
-        """更新外星人群中所有外星人的位置。"""
+        """检查是否有外星人位于屏幕边缘，
+        并更新整群外星人的位置"""
+        self._check_fleet_edges()
         self.aliens.update()
 
 
