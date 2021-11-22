cf86f80106beb1c9cae11c64d47172550eb7e839
张海浩 <zhanghaihao@singleronbio.com>
2021-11-22 11:33:06 +0800

    12.8.7 创建方法_update_bullets()

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -48,7 +48,7 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key ==pygame.K_q:
+        elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
@@ -62,13 +62,24 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """创建一颗子弹，并将其加入编组bullets中"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+        # 删除消失的子弹。
+        # for bullet in self.bullets.copy():
+        #     if bullet.rect.bottom <= 0:
+        #         self.bullets.remove(bullet)
+        # # print(len(self.bullets))
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹。"""
+        # 更新子弹的位置
+        self.bullets.update()
+
         # 删除消失的子弹。
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        # print(len(self.bullets))
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
